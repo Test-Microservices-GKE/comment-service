2bb302842d538342a9e8278d8e9db02a591be237
Trinh-Tan-Dat <21520714@gm.uit.edu.vn>
2024-05-24 14:38:36 +0700

    update

--- src/main/java/com/programming/streaming/controller/CommentController.java
+++ src/main/java/com/programming/streaming/controller/CommentController.java
@@ -2,7 +2,6 @@ package com.programming.streaming.controller;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ src/main/java/com/programming/streaming/controller/CommentModel.java
@@ -0,0 +1,5 @@
+package com.programming.streaming.controller;
+
+public class CommentModel {
+    
+}
